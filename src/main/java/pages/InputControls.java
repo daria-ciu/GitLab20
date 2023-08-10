7b1af00d494340ad8ec421a5c6c7b39d511ea294
user <daria.ciubancan@gmail.com>
2023-08-10 19:19:18 +0300

    Another commit

---
+++ src/main/java/pages/InputControls.java
@@ -0,0 +1,54 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class InputControls extends BasePage {
+    public InputControls (WebDriver driver) {
+        super(driver);}
+
+
+
+    By inStockBox = By.xpath("/html/body/div[1]/div[6]/div[1]/div[3]/div/div[2]/div[1]/div/div[2]/div[5]/div[2]/div/div[1]/div");
+    By preOrderBox = By.xpath("/html/body/div[1]/div[6]/div[1]/div[3]/div/div[2]/div[1]/div/div[2]/div[5]/div[2]/div/div[2]/div");
+    By blueCheckbox = By.xpath("/html/body/div[1]/div[6]/div[1]/div[3]/div/div[2]/div[1]/div/div[2]/div[4]/div[2]/div/div[1]/div/label/img");
+    By colorClearButton = By.xpath("/html/body/div[1]/div[6]/div[1]/div[3]/div/div[2]/div[1]/div/div[2]/div[4]/div[1]/a");
+    By appleSelector = By.xpath("/html/body/div[1]/div[6]/div[1]/div[3]/div/div[2]/div[1]/div/div[2]/div[2]/div[2]/div/div[1]/div/label");
+    By notActivePreOrderBox = By.xpath("/html/body/div[1]/div[6]/div[1]/div[3]/div/div[2]/div[1]/div/div[2]/div[5]/div[2]/div/div[2]/div");
+    By firstRadio = By.xpath("/html/body/div[1]/div[6]/div[1]/div[3]/div/div[2]/div[1]/div/div[2]/div[7]/div[2]/div/div[1]/div");
+    By sizeSelection = By.xpath("/html/body/div[1]/div[6]/div[1]/div[3]/div/div[2]/div[1]/div/div[2]/div[6]/div[2]/div/div[1]/div/label");
+
+
+
+    public void clickInStockBox (){driver.findElement(inStockBox).click();}
+    public  void clickPreOrderBox (){driver.findElement(preOrderBox).click();}
+    public void selectBlueCheckbox (){driver.findElement(blueCheckbox).click();}
+    public void waitForColorClear (){
+
+
+    WebDriverWait wait = new WebDriverWait(driver, 2);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(colorClearButton));}
+    public void clickColorClear (){waitForColorClear();driver.findElement(colorClearButton).click();}
+    public void clickAppleSelector (){driver.findElement(appleSelector).click();}
+    public void clickNotActivePreOrderButton (){driver.findElement(notActivePreOrderBox).click();}
+    public void clickFirstradio (){driver.findElement(firstRadio).click();}
+
+    public void selectSize (){driver.findElement(sizeSelection).click();}
+    public boolean AppleSelector (){return driver.findElement(appleSelector).isEnabled();}
+    public boolean ColorBlue (){return  driver.findElement(blueCheckbox).isEnabled();}
+    public boolean ColorClear (){return  driver.findElement(colorClearButton).isEnabled();}
+    public boolean selectFirstCheckbox (){return  driver.findElement(inStockBox).isEnabled();}
+    public boolean selectInactiveCheckbox (){return  driver.findElement(preOrderBox).isSelected();}
+    public boolean selectSecondCheckbox () {return  driver.findElement(preOrderBox).isEnabled();}
+    public boolean SelectSize (){return  driver.findElement(sizeSelection).isEnabled();}
+    public boolean selectRadio (){return driver.findElement(firstRadio).isSelected();}
+
+
+
+
+
+
+}
---
+++ src/test/java/testCases/InputControlsTestCase.java
@@ -0,0 +1,108 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.BasePage;
+import pages.InputControls;
+
+public class InputControlsTestCase
+        extends BasePage {
+    private InputControls inputControls;
+
+    @BeforeClass
+    public void setUp() {
+        super.setUp();
+
+        driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=product/category&path=28");
+    }
+
+    @BeforeMethod
+    public void navigateToCategoryPage() {
+        inputControls = new InputControls(driver);
+
+    }
+
+
+    @Test(priority = 1)
+    public void testSelectAppleOption() throws InterruptedException{
+        inputControls.clickAppleSelector();
+
+boolean appleSelector = inputControls.AppleSelector();
+
+        Assert.assertTrue(appleSelector,"Apple Selector is not enabled." );
+
+
+
+
+
+    }
+
+    @Test(priority = 2)
+    public void testColorSelection() throws InterruptedException{
+        inputControls.selectBlueCheckbox();
+
+        boolean isColorBlueSelected = inputControls.ColorBlue();
+        Assert.assertTrue(isColorBlueSelected, "Color blue is not selected.");
+    }
+
+    @Test (priority = 3)
+
+    public void testClearColorButton () throws InterruptedException{
+        inputControls.selectBlueCheckbox();
+        inputControls.clickColorClear();
+
+        boolean isColorCleared = inputControls.ColorClear();
+        Assert.assertTrue(isColorCleared, "Color is not cleared.");
+
+    }
+
+    @Test(priority = 4)
+
+    public void testFirstCheckbox () throws InterruptedException{
+    inputControls.clickInStockBox();
+
+    boolean isFirstCheckboxSelected = inputControls.selectFirstCheckbox();
+    Assert.assertTrue(isFirstCheckboxSelected, "The Checkbox is not selected.");}
+
+    @Test (priority = 5)
+
+    public void testMultipleCheckbox () throws InterruptedException{
+        inputControls.clickInStockBox();
+        inputControls.clickPreOrderBox();
+
+        boolean isInStockSelected = inputControls.selectFirstCheckbox();
+        boolean isPreOrderSelected = inputControls.selectSecondCheckbox();
+
+        Assert.assertTrue(isInStockSelected && isPreOrderSelected, "Both checkboxes are selected.");
+    }
+
+    @Test (priority = 6)
+
+    public void testSizeSelection () throws  InterruptedException{
+
+        inputControls.selectSize();
+
+        boolean isSizeSelected = inputControls.SelectSize();
+        Assert.assertTrue(isSizeSelected, "Size is not selected.");
+    }
+
+    @Test(priority = 7)
+
+    public void testInactiveRadioButton() throws InterruptedException{
+        inputControls.clickFirstradio();
+        boolean isNotActiveSelected = inputControls.selectRadio();
+        Assert.assertFalse(isNotActiveSelected, "Radio button should not be selectable.");
+    }
+
+    @Test (priority = 8)
+
+    public void testInactiveCheckbox() throws InterruptedException{
+        inputControls.clickAppleSelector();
+        inputControls.clickNotActivePreOrderButton();
+
+        boolean isInactiveCheckboxSelected = inputControls.selectInactiveCheckbox();
+        Assert.assertFalse(isInactiveCheckboxSelected, "Checkbox should not be selectable.");
+    }
+}
